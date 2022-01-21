8665988514a49ae65bbdf3aaafafe66a5b371da0
cedric-cha <cedric.chasseriau@gmail.com>
2022-01-21 16:38:57 +0000

    commit ++

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,16 +1,14 @@
 <footer class="footer py-4">
     <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-4 offset-lg-4 text-lg" style="justify-content:center;">Copyright &copy; Your Website 2021</div>
-            <!-- <div class="col-lg-4 my-3 my-lg-0">
-                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-            </div> 
-            <div class="col-lg-4 text-lg">
-                <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-                <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
-            </div> -->
+        <div class="">
+            <p style="text-align: center;">
+                Copyright &copy; 2022 - Tous droits reservés - Créé par
+                <a href="com-tic.africa">COM-TIC</a>
+            </p>
+            <p style="text-align: center;">
+                <a class="link-dark text-decoration-none me-3" href="#!">Mentions légales -</a>
+                <a class="link-dark text-decoration-none" href="#!">Politique de confidentialité</a>
+            </p>
         </div>
     </div>
 </footer>
